e49cf6c9bbba3515382c02c49b3f08048f9641a5
Vincent Hengel <prelink835@gmail.com>
2025-10-13 20:59:36 +0200

    Improve icons for new chat/folder

--- apps/web/src/components/SidebarNewChat.tsx
+++ apps/web/src/components/SidebarNewChat.tsx
@@ -8,7 +8,8 @@ export default function SidebarNewChat(props: Props) {
     <div class="newchat-dropdown">
       <button class="newchat" onClick={props.onClick}>
         <svg viewBox="0 0 16 16">
-          <path d="M6 3.5A5.5 5.5 0 0 1 14.5 8h-3.673A2.18 2.18 0 0 0 6.22 6.096L4.16 8.16a.75.75 0 0 1-1.061-1.061l2.064-2.064A2.18 2.18 0 0 0 3.673 5.5H.5A5.5 5.5 0 0 1 6 3.5zM1.5 8a5.5 5.5 0 0 1 8.5-4.673V.5a.75.75 0 0 1 1.5 0v3.827A5.5 5.5 0 0 1 1.5 8zm0 0h3.673a2.18 2.18 0 0 0 1.947 1.404l2.064-2.064a.75.75 0 0 1 1.061 1.061l-2.064 2.064A2.18 2.18 0 0 0 9.827 13.5H13.5a.75.75 0 0 1 0 1.5h-3.827A5.5 5.5 0 0 1 1.5 8z" />
+          <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2z"/>
+          <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
         </svg>
         New Chat
         <svg viewBox="0 0 12 12" style="margin-left: 4px;">
--- apps/web/src/components/SidebarNewFolder.tsx
+++ apps/web/src/components/SidebarNewFolder.tsx
@@ -6,8 +6,9 @@ export default function SidebarNewFolder(props: Props) {
   return (
     <button class="newfolder" onClick={props.onClick}>
       <svg viewBox="0 0 16 16">
-        <path d="M2 3.5A2.5 2.5 0 0 1 4.5 1h7A2.5 2.5 0 0 1 14 3.5v9a2.5 2.5 0 0 1-2.5 2.5h-7A2.5 2.5 0 0 1 2 12.5v-9zM4.5 2A1.5 1.5 0 0 0 3 3.5v9A1.5 1.5 0 0 0 4.5 14h7a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 11.5 2h-7z"/>
-        <path d="M8 5a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 5z"/>
+        <path d="M0 2a2 2 0 0 1 2-2h5.5L8 2.5H14a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 3.5H1v10.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V5.5z"/>
+        <path d="M4 1a1 1 0 0 0-1 1v2.5H2V2a2 2 0 0 1 2-2h5.5L10 2.5H14a1 1 0 0 1 1 1v1h-1V4H9.5L8 2.5H4a1 1 0 0 0-1 1z"/>
+        <path d="M8 7a.5.5 0 0 1 .5.5v2h2a.5.5 0 0 1 0 1h-2v2a.5.5 0 0 1-1 0v-2h-2a.5.5 0 0 1 0-1h2v-2A.5.5 0 0 1 8 7z"/>
       </svg>
       New folder
     </button>
